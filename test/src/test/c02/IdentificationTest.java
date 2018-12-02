819faf185aca42a60427ec1db027cd8ae8a47a21
inest-us <hieung@live.com>
2018-12-01 19:09:07 -0800

    Add unit tests

---
+++ test/src/test/c02/IdentificationTest.java
@@ -0,0 +1,17 @@
+package test.c02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import c02.Identification;
+
+class IdentificationTest {
+	private Identification sut = new Identification();
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
