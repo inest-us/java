985c49f13589f303e949024528189a37b4516425
inest-us <hieung@live.com>
2019-02-16 00:37:23 -0800

    TDD initial check-in

---
+++ tdd/src/us/inest/tdd/sample/MathTest.java
@@ -0,0 +1,17 @@
+package us.inest.tdd.sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+
+	@Test
+	void test() {
+		int a = 2;
+		int b = 3;
+		int c = a + b;
+		assertEquals(5, c);
+	}
+
+}
