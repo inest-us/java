39968ada0a654bec6d63cf69f819d0ad3d508572
Hieu Nguyen <hieung@live.com>
2019-05-27 00:57:57 -0700

    FindLCA and RebuildBSTFromPreorder

---
+++ epi/src/us/inest/epi/tree/RebuildBSTFromPreOrderTest.java
@@ -0,0 +1,24 @@
+package us.inest.epi.tree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import us.inest.epi.common.TreeNode;
+
+class RebuildBSTFromPreOrderTest {
+
+	@Test
+	void testRebuildBSTFromPreorder() {
+		List<Integer> arr = new ArrayList<Integer>(Arrays.asList(43, 23, 37, 29, 31, 41, 47, 53)); 
+		TreeNode root = RebuildBSTFromPreorder.rebuildBSTFromPreorder(arr);
+		assertEquals(43, root.data);
+		assertEquals(23, root.left.data);
+		assertEquals(47, root.right.data);
+	}
+
+}
---
+++ epi/src/us/inest/epi/tree/RebuildBSTFromPreorder.java
@@ -0,0 +1,24 @@
+package us.inest.epi.tree;
+
+import java.util.List;
+
+import us.inest.epi.common.TreeNode;
+
+public class RebuildBSTFromPreorder {
+	public static TreeNode rebuildBSTFromPreorder (List<Integer> arr) {
+		return rebuildBSTFromPreorderHelper(arr, 0, arr.size());
+	}
+
+	private static TreeNode rebuildBSTFromPreorderHelper(List<Integer> arr, int start, int end) {
+		if (start >= end) {
+			return null;
+		}
+		int transitionPoint = start + 1;
+		while (transitionPoint < end && arr.get(transitionPoint) < arr.get(start)) {
+			transitionPoint++;
+		}
+		return new TreeNode(arr.get(start), 
+				rebuildBSTFromPreorderHelper(arr, start + 1, transitionPoint),
+				rebuildBSTFromPreorderHelper(arr, transitionPoint, end));
+	}
+}
