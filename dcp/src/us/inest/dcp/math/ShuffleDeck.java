ef671d9768ca0b63f47833896321cba95b3ec18a
inest-us <hieung@live.com>
2019-05-27 22:03:06 -0700

    ShuffleDeck

---
+++ dcp/src/us/inest/dcp/math/ShuffleDeck.java
@@ -0,0 +1,18 @@
+package us.inest.dcp.math;
+
+import java.util.Random;
+
+public class ShuffleDeck {
+	public static int[] shuffle(int[] arr) {
+		Random r = new Random();
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			int j = r.nextInt(n - i) + i;
+			//swap arr[i], arr[j]
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		return arr;
+	}
+}
---
+++ dcp/src/us/inest/dcp/math/ShuffleDeckTest.java
@@ -0,0 +1,16 @@
+package us.inest.dcp.math;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShuffleDeckTest {
+
+	@Test
+	void testShuffleDeck() {
+		int[] arr = new int[] {1, 4, 3, 2, 5};
+		arr = ShuffleDeck.shuffle(arr);
+		assertNotNull(arr);
+	}
+
+}
