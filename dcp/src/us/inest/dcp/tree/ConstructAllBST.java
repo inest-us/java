df5352ab379230c0dbb8a1c82bf38713e15e74cc
inest-us <hieung@live.com>
2019-05-27 23:37:09 -0700

    Construct all BSTs

---
+++ dcp/src/us/inest/dcp/tree/ConstructAllBST.java
@@ -0,0 +1,32 @@
+package us.inest.dcp.tree;
+import java.util.ArrayList;
+import java.util.List;
+import us.inest.dcp.common.TreeNode;
+
+public class ConstructAllBST {
+	public static List<TreeNode> constructAllBST(int n) {
+		return constructAllBSTHelper(1, n);
+	}
+
+	private static List<TreeNode> constructAllBSTHelper(int low, int high) {
+		List<TreeNode> trees = new ArrayList<>();
+		if (low > high) {
+			trees.add(null);
+			return trees;
+		}
+		
+		for (int i = low; i <= high; i++) {
+			List<TreeNode> left = constructAllBSTHelper(low, i - 1);
+			List<TreeNode> right = constructAllBSTHelper(i + 1, high);
+			
+			//for every possible left and right subtrees
+			for (TreeNode l : left) {
+				for (TreeNode r : right) {
+					TreeNode n = new TreeNode(i, l, r);
+					trees.add(n);
+				}
+			}
+		}
+		return trees;
+	}
+}
