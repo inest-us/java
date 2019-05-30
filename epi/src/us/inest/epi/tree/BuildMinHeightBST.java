1aff92d7f0a0a1f09deab5c0d33170af00b799ba
Hieu Nguyen <hieung@live.com>
2019-05-29 21:48:08 -0700

    BuildMinHeightBST

---
+++ epi/src/us/inest/epi/tree/BuildMinHeightBST.java
@@ -0,0 +1,21 @@
+package us.inest.epi.tree;
+
+import java.util.List;
+
+import us.inest.epi.common.TreeNode;
+
+public class BuildMinHeightBST {
+	public static TreeNode buildMinHeightBST(List<Integer> arr) {
+		return buildMinHeightBSTHelper(arr, 0, arr.size() - 1);
+	}
+
+	private static TreeNode buildMinHeightBSTHelper(List<Integer> arr, int start, int end) {
+		if (start > end) {
+			return null;
+		}
+		int mid = (start + end) / 2;
+		return new TreeNode(arr.get(mid), 
+				buildMinHeightBSTHelper(arr, start, mid - 1), 
+				buildMinHeightBSTHelper(arr, mid + 1, end));
+	}
+}
---
+++ epi/src/us/inest/epi/tree/BuildMinHeightBSTTest.java
@@ -0,0 +1,22 @@
+package us.inest.epi.tree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import us.inest.epi.common.TreeNode;
+
+class BuildMinHeightBSTTest {
+
+	@Test
+	void testBuildMinHeightBST() {
+		List<Integer> arr = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23)); 
+		TreeNode root = BuildMinHeightBST.buildMinHeightBST(arr);
+		assertEquals(11, root.data);
+		assertEquals(3, root.left.data);
+		assertEquals(17, root.right.data);
+	}
+}
