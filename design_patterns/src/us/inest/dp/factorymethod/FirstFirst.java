4c5a902e436dea87ca54ac765cd3d919f4ff5611
inest-us <hieung@live.com>
2019-08-11 18:21:04 -0700

    Sample FactoryMethod

---
+++ design_patterns/src/us/inest/dp/factorymethod/FirstFirst.java
@@ -0,0 +1,18 @@
+package us.inest.dp.factorymethod;
+
+public class FirstFirst extends Namer { 
+	//split first last
+	public FirstFirst(String s) {
+		int i = s.lastIndexOf(" "); //find sep space
+		if (i > 0) {
+			//left is first name
+			first = s.substring(0, i).trim();
+			//right is last name
+			last = s.substring(i+1).trim();
+		}
+		else {
+			first = ""; // put all in last name if no space
+			last = s;
+		}
+	}
+}
