2b86814aac8179c03e441016dbbbc6899bea9b15
inest-us <hieung@live.com>
2019-12-18 23:03:51 -0800

    Dynamic Programming
    1. Total unique ways to make change from coins

---
+++ dcp/src/test/java/us/inest/app/dcp/dp/UniqueWaysToMakeChangeTest.java
@@ -0,0 +1,15 @@
+package us.inest.app.dcp.dp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UniqueWaysToMakeChangeTest {
+    @Test
+    public void testUniqueWaysToMakeChange() {
+        int amount = 5;
+        int[] coins = new int[] {1, 2, 5};
+        
+        int result = UniqueWaysToMakeChange.uniqueWaysToMakeChange(amount, coins);
+        assertEquals(4, result);
+    }
+}
