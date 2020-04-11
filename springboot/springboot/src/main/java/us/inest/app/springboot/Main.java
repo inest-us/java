9387c66280143590211499a301788ccf2a806d28
inest-us <hieung@live.com>
2020-04-11 10:25:27 -0700

    Enable JpaRepositories

--- springboot/springboot/src/main/java/us/inest/app/springboot/Main.java
+++ springboot/springboot/src/main/java/us/inest/app/springboot/Main.java
@@ -2,12 +2,14 @@ package us.inest.app.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication //enable component-scanning and and auto-configuration
+@EnableJpaRepositories
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args); //bootstrap the application
 
     }
     
--- springboot/springboot/src/main/java/us/inest/app/springboot/RestfulResource.java
+++ springboot/springboot/src/main/java/us/inest/app/springboot/RestfulResource.java
@@ -1,14 +1,22 @@
 package us.inest.app.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestfulResource {
+    private ReadingListRepository readingListRepository;
+
+    @Autowired
+    public RestfulResource(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
     @RequestMapping(value = "/api")
     public String api() {
         return "REST API";
     }
-    
+
     @RequestMapping(value = "/")
     public String hello() {
         return "Hello World";
